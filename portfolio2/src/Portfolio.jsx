e6b1b78c93bbf97c79a4dd41aafba069aac6248c
Gomolemo Motlhamme <gmotlhamme@jhb.dvt.co.za>
2025-04-14 14:54:07 +0200

    Add Projects and MoreInfoModal component for displaying additional information in a modal overlay

--- portfolio2/src/Portfolio.jsx
+++ portfolio2/src/Portfolio.jsx
@@ -30,10 +30,30 @@ const darkMode = () => {
     body.style.color = "black";
 }
 
+const popOutBtn = document.querySelector("#pop-out-btn");
+function displayModal() {
+    popOutBtn.showModal();
+}
+
+function closeModal() {
+    popOutBtn.close();
+}
+
+// Prevent closing modal when clicking inside form
+// form.addEventListener("click", function (e) {
+//   e.stopPropagation();
+// });
+
+// Close modal when clicking outside the form
+// wrapper.addEventListener("click", function () {
+//   closeModal();
+// });
+
+
     return (
     
     <>
-        <header className='bg-white dark:bg-black p-5 m-2 rounded-lg h-170'>
+        <header className="bg-white dark:bg-[url('./black-spheres.jpg')] bg-cover bg-right p-5 m-2 rounded-lg h-170">
             <section className='flex items-center justify-between p-5'>
 
                 <div id='logo-light' className='hidden dark:block w-35 -mr-15'>
@@ -47,10 +67,10 @@ const darkMode = () => {
 
             <nav className='dark:text-white hidden md:flex'>
                 <ul className='flex gap-30 text-md font-semibold '>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a href="#services">SERVICES</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a href="#about">ABOUT</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a href="#projects">PROJECTS</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a href="#technologies">TECHNOLOGIES</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a href="#services">SERVICES</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a href="#about">ABOUT</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a href="#projects-banner">PROJECTS</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a href="#technologies">TECHNOLOGIES</a></li>
                 </ul>
             </nav>
 
@@ -60,22 +80,22 @@ const darkMode = () => {
             <section id="sidebar" className=" hidden">
 
             <nav  className='bg-zinc-200  dark:bg-zinc-800 p-4 w-80 dark:text-white flex flex-col justify-end absolute top-10 right-10 z-2 '>
-                <button onClick={sidebarBtn} className="flex justify-end m-4 cursor-pointer dark:font-light hover:text-indigo-600 dark:hover:text-green-500">CLOSE</button>
+                <button onClick={sidebarBtn} className="flex justify-end m-4 cursor-pointer dark:font-light hover:text-indigo-600 dark:hover:text-amber-600">CLOSE</button>
                 <hr />
                 <ul className='flex flex-col gap-10 m-4 my-5 text-md dark:font-light'> 
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a onClick={sidebarBtn} href="#services">SERVICES</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a onClick={sidebarBtn} href="#about">ABOUT</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a onClick={sidebarBtn} href="#projects">PROJECTS</a></li>
-                    <li className='hover:text-indigo-600 dark:hover:text-green-500'><a onClick={sidebarBtn} href="#technologies">TECHNOLOGIES</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a onClick={sidebarBtn} href="#services">SERVICES</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a onClick={sidebarBtn} href="#about">ABOUT</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a onClick={sidebarBtn} href="#projects-banner">PROJECTS</a></li>
+                    <li className='hover:text-indigo-600 dark:hover:text-amber-600'><a onClick={sidebarBtn} href="#technologies">TECHNOLOGIES</a></li>
                     <hr />
                 
                     <div className="dark:font-light ">
                         
-                        <button onClick={changeAppearence}  className="dark:font-light hover:text-indigo-600 dark:hover:text-green-500 cursor-pointer">APPEARENCE</button>
+                        <button onClick={changeAppearence}  className="dark:font-light hover:text-indigo-600 dark:hover:text-amber-600 cursor-pointer">APPEARENCE</button>
                         
                         <div  id="toggle-container" className="invisible mt-2">
-                            <p onClick={lightMode} className="p-2 ml-7 hover:text-indigo-600 dark:hover:text-green-500 cursor-pointer">LIGHT</p>
-                            <p onClick={darkMode} className="p-2 ml-7 hover:text-indigo-600 dark:hover:text-green-500 cursor-pointer">DARK</p>
+                            <p onClick={lightMode} className="p-2 ml-7 hover:text-indigo-600 dark:hover:text-amber-600 cursor-pointer">LIGHT</p>
+                            <p onClick={darkMode} className="p-2 ml-7 hover:text-indigo-600 dark:hover:text-amber-600 cursor-pointer">DARK</p>
                         </div>
                         
                     </div>
@@ -98,32 +118,32 @@ const darkMode = () => {
         <main className=' dark:bg-transparent m-2 grid grid-cols-2 gap-5 mb-75'>
 
             {/* services section  */}
-        <section id='services' className='flex rounded-lg justify-end p-5 font-semibold bg-white dark:bg-black dark:text-white h-110 text-5xl '>EXPLORE MY <br></br> SERVICES</section>
+        <section id='services' className='flex rounded-lg justify-end p-5 font-semibold bg-white dark:bg-[url("./blend.jpg")] bg-cover dark:text-white h-110 text-5xl '>EXPLORE MY <br></br> SERVICES</section>
 
 
         <section className=' bg-transparent overflow-y-scroll hidescroll flex flex-col gap-2.5 h-110'>
 
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>WEB DEVELOPMENT 
             <p className='text-lg flex justify-end mt-11 mr-4 gap-2 cursor-pointer dark:text-white'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>WEB DEVELOPMENT <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>WEB DEVELOPMENT <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
 
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out  dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>FULL STACK DEVELOPMENT <p className='text-lg  flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>FULL STACK DEVELOPMENT <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>FULL STACK DEVELOPMENT <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
 
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out  dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>CYBERSECURITY <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
             <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p>
             </div>
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out  dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>CYBERSECURITY <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
             <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p>
             </div>
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out  dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>CYBERSECURITY <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
             <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p>
             </div>
             <div className='bg-white text-4xl hover:bg-linear-65 from-sky-500 to-indigo-500 duration-300 ease-in-out  dark:text-white dark:bg-black dark:hidden w-full h-35 p-4 rounded-lg'>CYBERSECURITY <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p></div>
-            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-green-600 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
+            <div className='bg-white text-4xl hover:bg-linear-90 hidden from-black-500 to-amber-700 duration-300 ease-in-out  dark:text-white dark:bg-black dark:block w-full h-35 p-4 rounded-lg'>CYBERSECURITY
             <p className='text-lg flex justify-end mt-11 mr-4 gap-2 dark:text-white cursor-pointer'>LEARN MORE <i className="bi bi-arrow-up-right-square "></i></p>
             </div>
         </section>
@@ -135,16 +155,44 @@ const darkMode = () => {
 Beyond code, I care deeply about clean design, performance, and solving real-world problems through technology. I've collaborated on projects that range from improving company websites to building systems from scratch, and I enjoy taking ownership from concept to deployment.</p>
         </article>
 
-        <section id='projects-banner' className='bg-white dark:bg-black rounded-lg h-50 col-span-full flex justify-between p-5'>
+        <section id='projects-banner' className='bg-white dark:bg-[url("./blend.jpg")] bg-cover bg-center rounded-lg h-50 col-span-full flex justify-between p-5'>
             <p className='dark:text-white flex justify-end text-xl font-normal place-items-center h-full mt-12 '>BELOW ARE A FEW OF MY PROJECTS <br></br> TO PROVE MY CAPABILITIES</p>
             <p className='dark:text-white flex justify-end text-8xl font-semibold place-items-center pb-9 h-full'>PROJECTS</p>
         </section>
 
-        <section id="projects" className='rounded-lg h-50 col-span-full grid grid-cols-3 gap-2'>
-            <div className='bg-white dark:text-white dark:bg-black rounded-lg h-60 col-span-2'>b</div>
-            <div className='bg-white dark:text-white dark:bg-black rounded-lg h-60'>b</div>
-            <div className='bg-white dark:text-white dark:bg-black rounded-lg h-60'>b</div>
-            <div className='bg-white dark:text-white dark:bg-black rounded-lg h-60 col-span-2'>b</div>
+        <section id="projects" className='rounded-lg h-50 col-span-full grid grid-cols-3 gap-2 '>
+            <div id="first-project" className='bg-white dark:text-white dark:bg-black font-semibold flex justify-around items-center rounded-lg h-60 col-span-2 p-5'>
+                <span className="text-5xl font-medium">Subreddit<br></br> Channel <br></br> Viewer</span>
+                <div className="gap-3 flex flex-col ">
+                <p className='text-xl font-light'>A web application that allows users <br></br> to view and interact with Reddit channels,<br></br> showcasing my skills in API integration<br></br> and user interface design.</p>
+                <button className="border-2 dark:border-amber-200 dark:hover:text-amber-300 cursor-pointer px-2 rounded "><a href="https://gomoatdvt.github.io/Subreddit/" className="">VIEW</a></button>
+                </div>
+            </div>
+
+            <div id="second-project" className='bg-white dark:text-white dark:bg-black flex flex-col justify-around items-center text-center rounded-lg p-3 h-60'>
+            <span className="text-3xl">SORTED</span>
+                <div className="gap-3 flex flex-col px-11">
+                <p className='text-base  font-light text-start'>A web application that allows users to visually organise tasks they have to complete, improving time management and productivity. Showcasing my skills in DOM manipulation in JavaScript and user interface design.</p>
+                <button className="border-2 dark:border-amber-200 dark:hover:text-amber-300 cursor-pointer  w-full rounded "><a href="https://gomoatdvt.github.io/To-Do-list/" className="">VIEW</a></button>
+                </div>
+            </div>
+
+            <div id="third-project" className='bg-white dark:text-white dark:bg-black flex flex-col items-center text-center p-3 justify-around rounded-lg h-60'>
+            <span className="text-3xl">Age Calculator</span>
+                <div className="gap-3 flex flex-col px-11 ">
+                <p className='text-base  font-light text-start'>A web application that allows users to recieve an accurate calculation of their, or any other person/ things age. Showcasing my skills in Node package management, DOM manipulation in JavaScript and user interface design.</p>
+                <button className="border-2 dark:border-amber-200 dark:hover:text-amber-300 cursor-pointer  w-full rounded "><a href="https://gomoatdvt.github.io/Age-calculator/" className="">VIEW</a></button>
+                </div>
+            </div>
+
+            <div id="fourth-project" className='bg-white dark:text-white dark:bg-black flex justify-around items-center rounded-lg py-6 col-span-2'>
+            <span className="text-5xl font-medium"> Blogger <br />Cloud</span>
+                <div className="gap-3 flex flex-col ">
+                <p className='text-xl font-light'>A web application that allows users <br></br> to view, create, post, and delete blogs.<br></br> showcasing my skills in DOM Manipulation<br></br> and user interface design.</p>
+                <button className="border-2 dark:border-amber-200 dark:hover:text-amber-300 cursor-pointer px-2 rounded "><a href="#" className="">VIEW</a></button>
+                </div>
+            </div>
+
         </section>
         </main>
 
@@ -182,7 +230,7 @@ Beyond code, I care deeply about clean design, performance, and solving real-wor
         country={"za"}
         value={phone}
         onChange={(phone) => setPhone(phone)}
-    />
+    />.
 </div>
 
 
@@ -192,11 +240,11 @@ Beyond code, I care deeply about clean design, performance, and solving real-wor
                         <textarea type="text" placeholder='Type your message' className='p-2 inset-shadow-sm border border-zinc-200 bg-white text-black h-50 rounded'></textarea>
                         </div>
 
-                        <button className='text-white bg-black h-10 rounded mt-5 cursor-pointer shadow-md active:shadow-none hover:text-blue-500 dark:hover:border-green-400  dark:hover:text-green-500 dark:text-green'>Send Message</button>
+                        <button className='text-white bg-black h-10 rounded mt-5 cursor-pointer shadow-md active:shadow-none hover:text-blue-500 dark:hover:border-amber-400  dark:hover:text-amber-500 dark:text-green'>Send Message</button>
 
                 </form>
             </section>
-            <section id='technologies' className='bg-blue-600 dark:bg-green-500 rounded-lg col-1 row-1 md:col-2 '>b</section>
+            <section id='technologies' className='bg-blue-600 dark:bg-[url("./peach-cube.jpg")] bg-cover rounded-lg col-1 row-1 md:col-2 '>b</section>
         </footer>
     </>
     
