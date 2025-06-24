46923d7a5abfabaeb83c32ccbb4b95507b7e9b20
Gomolemo Motlhamme <gmotlhamme@jhb.dvt.co.za>
2025-06-24 15:56:09 +0200

    feat: enhance portfolio components with responsive design and improved functionality

--- portfolio2/src/components/ProjectsBanner.jsx
+++ portfolio2/src/components/ProjectsBanner.jsx
@@ -6,12 +6,12 @@ function ProjectsBanner() {
         <>
         <section
             id="projects-banner"
-            className='bg-[url("./soft-performance.jpg")] dark:bg-[url("./black-spheres.jpg")] bg-cover bg-center  rounded-lg h-50 col-span-full flex justify-between p-5'
+            className='bg-[url("./soft-performance.jpg")] dark:bg-[url("./black-spheres.jpg")] bg-cover bg-center  rounded-lg h-fit col-span-full flex justify-between p-5'
           >
-            <p className="animate-fadeIn4 dark:text-white flex justify-end text-xl font-normal place-items-center h-full mt-12 ">
+            <p className="animate-fadeIn4 dark:text-white flex justify-end lg:text-xl font-normal place-items-center h-fit lg:mt-18 sm:mt-10 sm:text-sm">
               BELOW ARE A FEW OF MY PROJECTS <br></br> TO PROVE MY CAPABILITIES
             </p>
-            <p className="animate-fadeIn3 dark:text-white flex justify-end text-8xl font-semibold place-items-center pb-9 h-full">
+            <p className="animate-fadeIn3 dark:text-white flex justify-center text-8xl  font-semibold place-items-center pb-9 h-fit">
               PROJECTS
             </p>
           </section>
